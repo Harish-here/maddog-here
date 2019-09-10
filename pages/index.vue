e3d920b179c2f63d73277fa63bb069f188466f1f
Harish Ravi <harishamudha@gmail.com>
2019-09-10 19:37:28 +0530

    layout done

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,19 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        maddog-here
-      </h1>
-      <h2 class="subtitle">
-        A portfolio by Harish aka maddog
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+    
+    <div class='flex flex-column'>
+    <span>we</span>
+    <span>e</span>
     </div>
+      
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+
 
 export default {
-  components: {
-    Logo
-  }
+ 
 }
 </script>
 
